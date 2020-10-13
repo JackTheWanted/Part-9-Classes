1721aff0e667387df55f214f42c28a61d88c9829
JackTheWanted <58087382+JackTheWanted@users.noreply.github.com>
2020-10-12 22:13:26 -0400

    g

--- Program.cs
+++ Program.cs
@@ -10,6 +10,46 @@ namespace Part_9_Classes
     {
         static void Main(string[] args)
         {
+ 
+            string firstName;
+            string lastName;
+
+
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student("Jack", "Carlson"));
+            students.Add(new Student("Cole", "Cantwell"));
+            students.Add(new Student("Tilso", "Beatie"));
+
+
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);                
+            }
+
+            do
+            {
+                Console.WriteLine("Please enter the first name of a student you wish to add.");
+                firstName = Console.ReadLine();
+
+                Console.WriteLine("Please enter the last name of the student you wish to add.");
+                lastName = Console.ReadLine();
+
+                students.Add(new Student(firstName, lastName));
+
+            } while (students.Count < 5);
+
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.StudentNumber);
+
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_9_Classes
+{
+    
+    class Student
+    {
+        private static Random generator = new Random();
+        private string firstName;
+        private string lastName;
+        private int studentNum;
+        private string email;
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName.Trim();
+            this.lastName = lastName.Trim();
+            studentNum = generator.Next(999) + 555000;
+            GenerateEmail();
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                this.firstName = value;
+                GenerateEmail();
+            }
+        }
+         public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                this.lastName = value;
+                GenerateEmail();
+            }
+        }
+        public int StudentNumber
+        {
+            get
+            {
+                return studentNum;
+            }
+        }
+        public void ResetStudentNumber()
+        {
+            this.studentNum = generator.Next(999) + 555000;
+            GenerateEmail();
+        }
+        public override string ToString()
+        {
+            return firstName + " " + lastName;
+        }
+        private void GenerateEmail()
+        {
+            string firstThree, lastThree;
+
+            if (firstName.Length <= 3)
+            {
+                firstThree = firstName;
+            }
+            else
+            {
+                firstThree = firstName.Substring(0, 3);
+            }
+
+            if (lastName.Length <= 3)
+            {
+                lastThree = lastName;
+            }
+            else
+            {
+                lastThree = lastName.Substring(0, 3);
+            }
+
+            email = firstThree + lastThree + (StudentNumber + "").Substring(3) + "@ICS4U.com";
+           
+            
+        }
+    }
+
+    
+
+
+
+}
